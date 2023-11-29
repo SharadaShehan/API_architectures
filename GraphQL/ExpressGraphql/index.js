0a0bf9fc712125fe37edd2f830107680fe953966
Sharada Shehan <sharadashehan6@gmail.com>
2023-11-29 14:56:42 +0530

    basic graphQL

--- GraphQL/ExpressGraphql/index.js
+++ GraphQL/ExpressGraphql/index.js
@@ -1,30 +1,68 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const crypto = require('crypto');
 
 
 const fakeDB = {
     users: [
         { id: "1", name: "John Doe", email: "John6@gmail.com" },
         { id: "2", name: "Sam Convoy", email: "Sam7@gmail.com" },
+    ],
+    messages : [
+        { id: "1", userId: "1", body: "Hello World" },
+        { id: "2", userId: "2", body: "Bye World" },
+        { id: "3", userId: "1", body: "Hey World" },
     ]
 };
 
 const schema = buildSchema(`
     type Query {
         users: [User!]!
+        messages: [Message!]!
+        user(id: ID!): User
+    }
+
+    type Mutation {
+        createUser(name: String!, email: String!): User
     }
 
     type User {
         id: ID!
         name: String!
         email: String!
+        messages: [Message!]!
+    }
+
+    type Message {
+        id: ID!
+        body: String!
+        user: User!
     }
 `);
 
+class User {
+    constructor(user) {
+        Object.assign(this, user);
+    }
+
+    messages() {
+        return fakeDB.messages.filter(message => message.userId === this.id);
+    }
+}
+
 const rootValue = {
     users: () => {
-        return fakeDB.users;
+        return fakeDB.users.map(user => new User(user));
+    },
+    user: ({ id }) => {
+        return new User(fakeDB.users.find(user => user.id === id));
+    },
+    createUser: ({ name, email }) => {
+        const id = crypto.randomBytes(10).toString('hex');
+        const newUser = { id, name, email };
+        fakeDB.users.push(newUser);
+        return newUser;
     }
 };
 
--- README.md
+++ README.md
@@ -8,11 +8,12 @@ npm init -y
 ## GrpghQL
 Remove dependencies from package.json
 ```
-npm i graphql
-npm i express
-npm i express-graphql
+npm i graphql@15.3.0
+npm i express@4.18.2
+npm i express-graphql@0.12.0
 npm install supervisor --save-dev
 ```
+
 update package.json
 ```
 "scripts": {
